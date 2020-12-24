8d8387dde1ae547d77f5f0f696126f74b9dc9540
NullPrograms <adoopie100@gmail.com>
2020-12-24 20:30:34 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+import random
+import string
+import requests
+f=open("Valid Nitro.txt", "w", encoding="utf-8")
+
+while True:
+    code = ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+    r = requests.get(f"https://discordapp.com/api/v7/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true")
+    if r.status_code == 200:
+        print(f"Working Code : discord.gift/{code}")
+        f.write(f"discord.gift/{code}\n")
+    else:
+        print(f"Not Working Nitro Code : discord.gift/{code}")
